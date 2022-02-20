1b5b090b5435f245ba2cbbc7ce8653be2f60e26e
showwin <showwin_kmc@yahoo.co.jp>
2022-02-20 11:51:07 +0900

    bump v1.1.5

--- speedtest.rb
+++ speedtest.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-SPEEDTEST_VERSION = '1.1.3'
+SPEEDTEST_VERSION = '1.1.5'
 class Speedtest < Formula
   desc 'speedtest cli'
   homepage 'https://github.com/showwin/speedtest-go'
