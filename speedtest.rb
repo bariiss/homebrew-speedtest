529ba94a6958fa2cbe1475ae03e3893631cfceab
r3inbowari <r3inbowari@gmail.com>
2024-06-12 23:45:15 +0800

    Release v1.7.7

--- speedtest.rb
+++ speedtest.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-SPEEDTEST_VERSION = '1.7.5'
+SPEEDTEST_VERSION = '1.7.7'
 class Speedtest < Formula
   desc 'speedtest cli'
   homepage 'https://github.com/showwin/speedtest-go'
